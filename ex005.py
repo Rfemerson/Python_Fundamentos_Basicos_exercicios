4159264564386d3668b1e5c2f08e8b74755cc76d
Rfemerson <emersonferreirar8@gmail.com>
2022-06-08 18:51:53 -0300

    ex005

---
+++ ex005.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número:'))
+n1 = n + 1
+n2 = n - 1
+print('O sucessor de {}, é {} e o antecessor é {}'.format(n, n1, n2))
+# print('Avaliando o valor {}, seu sucessor é {} e seu antecessor é {}'.format(n, (n + 1),(n - 1)))
