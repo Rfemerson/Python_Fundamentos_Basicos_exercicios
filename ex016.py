460d74fbfcabb4eaba738ce293b117b43e5a1c45
Rfemerson <emersonferreirar8@gmail.com>
2022-06-08 20:39:12 -0300

    Arredondando Números Reais

---
+++ ex016.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+import math
+nam = float(input('Digite um número:'))
+i = math.trunc(nam)
+print('A porção inteira do número {} é {}'.format(nam, i))
+print('A porção inteira do número {} é {}'.format(n, int(nam)))
