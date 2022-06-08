df90a0e63e9b87563b5fa36739ab3aca3ca6e710
Rfemerson <emersonferreirar8@gmail.com>
2022-06-08 20:30:41 -0300

    ex013

---
+++ ex013.py
@@ -0,0 +1,4 @@
+#faça um algoritmo que leia o salário de um funcionario e mostre seu novo salario com 15% de aumentos
+s = float(input('Qual seu salário atual?'))
+a = s + (s * 0.15)
+print('Seu novo salário será de {} reais'.format(a))
