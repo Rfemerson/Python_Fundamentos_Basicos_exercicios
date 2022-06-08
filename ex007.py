fd481886fd346e74f944663ccb861a997c51b779
Rfemerson <emersonferreirar8@gmail.com>
2022-06-08 19:04:52 -0300

    ex007

---
+++ ex007.py
@@ -0,0 +1,5 @@
+# Desenvolva um programa que leia as notas duas de um aluno, calcule e mostre a sua media
+n1 = float(input('Qual sua primeira nota?'))
+n2 = float(input('Qual sua segunda nota?'))
+m = (n1 + n2) / 2
+print('Sua média nessa matéria é {:1f}'.format(m))
