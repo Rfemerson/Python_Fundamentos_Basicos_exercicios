c16d1d57a6491f89103995662d810fbd7b53fb09
Rfemerson <emersonferreirar8@gmail.com>
2022-06-08 19:06:52 -0300

    ex008

---
+++ ex008.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um valor em metros e o exiba em centimetros e milimetros
+n = float(input('Digite o valor, em metros, que gostaria de converter:'))
+n1 = n * 100
+n2 = n * 1000
+print('{} centimentros'.format(n1))
+print('{} milimentros'.format(n2))
+
