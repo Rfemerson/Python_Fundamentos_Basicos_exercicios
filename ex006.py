3b0de5f7f971a02bf4f83382de63e5582df5907c
Rfemerson <emersonferreirar8@gmail.com>
2022-06-08 18:54:50 -0300

    ex006

---
+++ ex006.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
+n = int(input('Digite um número:'))
+n1 = n * 2
+n2 = n * 3
+n3 = n ** (1/2)
+print('O dobro do número é {}, \n O triplo do número é {}, a Raiz quadrada é {:2f}'.format(n1, n2, n3))
+
